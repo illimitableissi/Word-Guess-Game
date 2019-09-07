a801fbb686b198432041d083209c780035b5f407
SOVEREIGN <isimemeni@gmail.com>
2019-09-07 18:12:26 -0400

    added new game javascript code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,17 @@
 
 // Game Logics
-var maxTries = 6;
 var wrongLetters = [];
 var currentWordIndex = "";
-var remainingGuesses = 0;
 var answerDisplay = [];
 var gameStart = false;
 var gameFinish = false;
-var wins = false;
 var currentWrdLetters = [];
+var numBlanks = 0;
+
+// Stats
+var wins = 0;
+var losses = 0;
+var remainingGuesses = 8;
 
 
 // Array listing all words used in the game
@@ -20,6 +23,7 @@ var characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m
 
 // Variables that hold references to the places in the HTML.
 var displayWins = document.getElementById("display-wins");
+var displayLosses = document.getElementById("display-losses");
 var currentWord = document.getElementById("current-word");
 var guessesRemaining = document.getElementById("guesses-remaining");
 var alreadyGuessed = document.getElementById("already-guessed");
@@ -27,6 +31,7 @@ var alreadyGuessed = document.getElementById("already-guessed");
 
 // New Game
 function newGame() {
+
     // Generates random word from words array
     currentWordIndex = words[Math.floor(Math.random() * words.length)];
         console.log("The current word is: " + currentWordIndex);
@@ -35,5 +40,22 @@ function newGame() {
         console.log("The number of letters are: " + currentWrdLetters)
     
     numBlanks = currentWrdLetters.length;
-        console.log("number of letters " + numBlanks)
+        console.log("The number of listters in current word: " + numBlanks);
+
+    guessesRemaining = 8;
+    wrongLetters = [];
+    answerDisplay = [];
+
+    for (i = 0; i < numBlanks; i++) {
+        answerDisplay.push("_")
+    }
+
+    currentWord.textcontent = answerDisplay.join(" ");
+    guessesRemaining.textcontent = "Number of Guesses Remaining: " + " " + remainingGuesses
+    displayWins.textContent = "Wins: " + " " + wins;
+    displayLosses.textcontent = "Losses: " + " " + losses;
+        
 }
+
+
+
