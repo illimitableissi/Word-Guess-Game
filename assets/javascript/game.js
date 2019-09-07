44a807fc0e7115bf29c8d6631eb12ffc09cc0351
SOVEREIGN <isimemeni@gmail.com>
2019-09-06 21:40:48 -0400

    declared variables in js sheet

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+
+    // Array listing every letter of the alphabet
+    var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
+
+
+    // Create variables that hold references to the places in the HTML where we want to display things.
+    var directiontext = document.getElementById("directions text");
+    var displaywins = document.getElementById("display-wins");
+    var guessesremaining = document.getElementById("guesses-remaining");
+    var alreadyguessed = document.getElementById("already-guessed");
+
+
+    // This function is run whenever the user presses a key.
+    document.onkeyup = function(event) {}
+
+
