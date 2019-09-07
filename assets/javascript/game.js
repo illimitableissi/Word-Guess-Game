efbc2f4d3c48977293ded30d5e413d6a5cd2716a
SOVEREIGN <isimemeni@gmail.com>
2019-09-07 00:54:48 -0400

    added game logics

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,14 @@
 
-// Array listing total number of words
-var NumberOfWords = 5
+// Game Logics
+var maxTries = 10;
+var guessedLetters = [];
+var currentWordIndex;
+var remainingGuesses = 0;
+var guessingWord = [];
+var gameStart = false;
+var gameFinish = false;
+var wins = false;
+
 
 // Array listing all words used in the game
 var words = ["reunion", "genesis", "overload", "maximum", "sovereign"]
@@ -9,19 +17,25 @@ var words = ["reunion", "genesis", "overload", "maximum", "sovereign"]
 var characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
 
 
-// Create variables that hold references to the places in the HTML where we want to display things.
+// Variables that hold references to the places in the HTML.
 var displayWins = document.getElementById("display-wins");
 var currentWord = document.getElementById("current-word");
 var guessesRemaining = document.getElementById("guesses-remaining");
 var alreadyGuessed = document.getElementById("already-guessed");
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
 
-currentWord.textContent = words[Math.floor(Math.random() * words.length)];
+// On Game Start Up
 
-// Shows letter user presses under "Letters Already Guessed"
-document.onkeyup = function(event) {
-    alreadyGuessed.textContent = event.key
+
+
+function Hangman() {
+
+// Generates random word from words array
+currentword = words[Math.floor(Math.random() * words.length)];
+
+// Creating answer array loop
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {}
+
+
+
 }
-
-    
