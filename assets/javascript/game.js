5fc7b016a14f9933bfaaf2f525467605bd2d78fc
SOVEREIGN <isimemeni@gmail.com>
2019-09-06 23:21:29 -0400

    added random word generator code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,23 +3,25 @@
 var NumberOfWords = 5
 
 // Array listing all words used in the game
-var words = ["reunion", "genesis", "overload", "maximum", "soveriegn"]
+var words = ["reunion", "genesis", "overload", "maximum", "sovereign"]
 
 // Array listing every letter of the alphabet
 var characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
 
 
 // Create variables that hold references to the places in the HTML where we want to display things.
-var directiontext = document.getElementById("directions text");
-var displaywins = document.getElementById("display-wins");
-var guessesremaining = document.getElementById("guesses-remaining");
-var alreadyguessed = document.getElementById("already-guessed");
-var winstext = document.getElementById("wins-text");
-var lossestext = document.getElementById("losses-text");
+var displayWins = document.getElementById("display-wins");
+var currentWord = document.getElementById("current-word");
+var guessesRemaining = document.getElementById("guesses-remaining");
+var alreadyGuessed = document.getElementById("already-guessed");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
 
+currentWord.textContent = words[Math.floor(Math.random() * words.length)];
 
-// This function is run whenever the user presses a key.
+// Shows letter user presses under "Letters Already Guessed"
 document.onkeyup = function(event) {
-    alreadyguessed.textContent = event.key
+    alreadyGuessed.textContent = event.key
 }
-   
+
+    
