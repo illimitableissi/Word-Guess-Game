47855731631103d29ae383c051f7c33e409b4694
SOVEREIGN <isimemeni@gmail.com>
2019-09-06 22:01:55 -0400

    decalred word variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,29 @@
 
-    // Array listing every letter of the alphabet
-    var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
+// Array listing total number of words
+var NumberOfWords = 5
+// Array listing all words used in the game
+var words = ["reunion", "genesis", "overload", "maximum", "soveriegn"]
+
+// Array listing every letter of the alphabet
+var characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
 
 
-    // Create variables that hold references to the places in the HTML where we want to display things.
-    var directiontext = document.getElementById("directions text");
-    var displaywins = document.getElementById("display-wins");
-    var guessesremaining = document.getElementById("guesses-remaining");
-    var alreadyguessed = document.getElementById("already-guessed");
+// Create variables that hold references to the places in the HTML where we want to display things.
+var directiontext = document.getElementById("directions text");
+var displaywins = document.getElementById("display-wins");
+var guessesremaining = document.getElementById("guesses-remaining");
+var alreadyguessed = document.getElementById("already-guessed");
 
 
-    // This function is run whenever the user presses a key.
-    document.onkeyup = function(event) {}
+// This function is run whenever the user presses a key.
+document.onkeyup = function(event) {
+
+    Math.random().toString(36)
 
 
+
+
+    
+}
+
+   
